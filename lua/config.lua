7a067114cc2e993be43f392ce485f72e40f9adab
r47orr <razoroller529@gmail.com>
2023-01-21 00:22:17 -0300

    Corrigir configurações básicas e padrão; remover função desnecessária

--- lua/config.lua
+++ lua/config.lua
@@ -11,6 +11,7 @@ local defaultSettings = {
         },
         secondary = {
             "weapon_zm_pistol",
+            "weapon_ttt_glock",
             "weapon_zm_revolver"
         },
         equipment = {
@@ -53,52 +54,93 @@ local function = FetchConfig(fileGameDir, filePath, fileName, data)
     return settings
 end
 
-local function = FetchSetting(config, setting)
-    return config.setting || ""
-end
-
 local config = {}
 
 config.weapons = {
-    primary_weapons = {
-    "weapon_zm_mac10",
-    "weapon_zm_shotgun",
-    "weapon_ttt_m16",
-    "weapon_zm_rifle",
-    "weapon_zm_sledge",
-    "weapon_ttt_ak47",
-    "weapon_ttt_aug",
-    "weapon_ttt_tmp",
-    "weapon_ttt_galil",
-    "weapon_ttt_famas",
-    "weapon_ttt_sg550",
-    "weapon_ttt_sg552",
-    "weapon_ttt_m3",
-    "weapon_ttt_mp5",
-    "weapon_ttt_g3sg1",
-    "weapon_ttt_smg",
-    "weapon_ttt_mac10",
-    "m9k_acr",
-    "m9k_an94",
-    "m9k_ar15",
-    "m9k_g36c",
-    "m9k_honeybadger",
-    "m9k_mk17",
-    "m9k_spas12",
-    "m9k_sten",
-    "m9k_vector",
+    server_weapon_ents = {
+        primary = {
+            "weapon_ttt_ak47",
+            "weapon_ttt_aug",
+            "weapon_ttt_tmp",
+            "weapon_ttt_galil",
+            "weapon_ttt_famas",
+            "weapon_ttt_sg550",
+            "weapon_ttt_sg552",
+            "weapon_ttt_m3",
+            "weapon_ttt_mp5",
+            "weapon_ttt_g3sg1",
+            "weapon_ttt_smg",
+            "weapon_ttt_mac10",
+            "m9k_acr",
+            "m9k_an94",
+            "m9k_ar15",
+            "m9k_g36c",
+            "m9k_honeybadger",
+            "m9k_mk17",
+            "m9k_spas12",
+            "m9k_sten",
+            "m9k_vector",
+            "weapon_zm_mac10",
+            "weapon_zm_shotgun",
+            "weapon_ttt_m16",
+            "weapon_zm_rifle",
+            "weapon_zm_sledge",
+        },
+        secondary = {
+            "weapon_zm_pistol",
+            "weapon_zm_revolver",
+            "weapon_ttt_dual_elites",
+            "weapon_ttt_revolver",
+            "weapon_ttt_glock",
+            "weapon_ttt_p228",
+            "m9k_luger",
+            "m9k_m92beretta",
+            "m9k_tec9",
+            "m9k_usp",
+        },
+        equipment = {
+            "weapon_ttt_knife"
+        }
     },
-    secondary_weapons = {
-    "weapon_zm_pistol",
-    "weapon_zm_revolver",
-    "weapon_ttt_dual_elites",
-    "weapon_ttt_revolver",
-    "weapon_ttt_glock",
-    "weapon_ttt_p228",
-    "m9k_luger",
-    "m9k_m92beretta",
-    "m9k_tec9",
-    "m9k_usp",
+    // blacklisted weapons that shouldn't be allowed in the addon's functionality
+    blacklisted_ents = {
+        weapon_ttt_knife = true,
+        weapon_ttt_ak47 = true,
+        weapon_ttt_aug = true,
+        weapon_ttt_tmp = true,
+        weapon_ttt_galil = true,
+        weapon_ttt_famas = true,
+        weapon_ttt_sg550 = true,
+        weapon_ttt_sg552 = true,
+        weapon_ttt_m3 = true,
+        weapon_ttt_mp5 = true,
+        weapon_ttt_g3sg1 = true,
+        weapon_ttt_smg = true,
+        weapon_ttt_mac10 = true,
+        m9k_acr = true,
+        m9k_an94 = true,
+        m9k_ar15 = true,
+        m9k_g36c = true,
+        m9k_honeybadger = true,
+        m9k_mk17 = true,
+        m9k_spas12 = true,
+        m9k_sten = true,
+        m9k_vector = true,
+        weapon_ttt_p228 = true,
+        m9k_luger = true,
+        m9k_m92beretta = true,
+        m9k_tec9 = true,
+        m9k_usp = true,
+        weapon_ttt_dual_elites = true,
+        weapon_ttt_revolver = true
+    },
+    // add ULX/CAMI ranks that should have access for the weapons' menu
+    choice_allowed_ranks = {
+        user = true
+    },
+    // by default, only superadmins should have access to the addon's global settings and management
+    config_allowed_ranks = {
+        superadmin = true
     }
 }
 
